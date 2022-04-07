0a33a2d0c8871f58edb79c9f45e13d5efbe89395
Gleb Perveev <sanlovty@gmail.com>
2022-04-07 20:22:38 +0300

    feat(navigation): added route file and provided to approuter, mainscreen

--- lib/features/navigation/app_router.dart
+++ lib/features/navigation/app_router.dart
@@ -1,10 +1,13 @@
+import 'package:first_week/features/common/screens/colored_list/colored_list_screen_route.dart';
 import 'package:first_week/features/common/screens/main/main_screen_route.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   static const String mainScreen = '/';
+  static const String coloredListScreen = '/coloredlist';
 
   static final Map<String, Route Function(Object?)> routes = {
     mainScreen: (_) => MainScreenRoute(),
+    coloredListScreen: (_) => ColoredListScreenRoute(),
   };
 }
