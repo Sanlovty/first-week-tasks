70641a1137b9699c03e3799b00caa5c60c239729
Gleb Perveev <sanlovty@gmail.com>
2022-04-07 15:18:26 +0300

    feat(main-screen): added route file for mainscreen

---
+++ lib/features/common/screens/main/main_screen_route.dart
@@ -0,0 +1,6 @@
+import 'package:first_week/features/common/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainScreenRoute extends MaterialPageRoute<void> {
+  MainScreenRoute() : super(builder: (ctx) => const MainScreen());
+}
