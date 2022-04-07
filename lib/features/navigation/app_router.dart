fed6b385cfbee3e0115677f9b3b71255935ac199
Gleb Perveev <sanlovty@gmail.com>
2022-04-07 15:32:36 +0300

    refactor(app-router): name of root route is swapped with '/'

--- lib/features/navigation/app_router.dart
+++ lib/features/navigation/app_router.dart
@@ -2,7 +2,7 @@ import 'package:first_week/features/common/screens/main/main_screen_route.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
-  static const String mainScreen = '/main';
+  static const String mainScreen = '/';
 
   static final Map<String, Route Function(Object?)> routes = {
     mainScreen: (_) => MainScreenRoute(),
